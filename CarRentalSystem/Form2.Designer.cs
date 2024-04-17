6c7a7e20338c4bdc5a830b0ce7c25b7da4317658
shehansasmik <shehansasmika02@gmail.com>
2024-04-18 01:28:13 +0530

    Dash board

--- CarRentalSystem/Form2.Designer.cs
+++ CarRentalSystem/Form2.Designer.cs
@@ -28,20 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.butCustomers = new System.Windows.Forms.Button();
+            this.butVehicles = new System.Windows.Forms.Button();
+            this.butBookings = new System.Windows.Forms.Button();
+            this.butReturns = new System.Windows.Forms.Button();
+            this.butDamages = new System.Windows.Forms.Button();
+            this.butLogout = new System.Windows.Forms.Button();
+            this.lblDashboard = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(49)))), ((int)(((byte)(96)))));
+            this.panel1.Controls.Add(this.butLogout);
+            this.panel1.Controls.Add(this.butDamages);
+            this.panel1.Controls.Add(this.butReturns);
+            this.panel1.Controls.Add(this.butBookings);
+            this.panel1.Controls.Add(this.butVehicles);
+            this.panel1.Controls.Add(this.butCustomers);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 454);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // butCustomers
+            // 
+            this.butCustomers.Location = new System.Drawing.Point(44, 59);
+            this.butCustomers.Name = "butCustomers";
+            this.butCustomers.Size = new System.Drawing.Size(85, 39);
+            this.butCustomers.TabIndex = 1;
+            this.butCustomers.Text = "Customers";
+            this.butCustomers.UseMnemonic = false;
+            this.butCustomers.UseVisualStyleBackColor = true;
+            // 
+            // butVehicles
+            // 
+            this.butVehicles.Location = new System.Drawing.Point(44, 130);
+            this.butVehicles.Name = "butVehicles";
+            this.butVehicles.Size = new System.Drawing.Size(85, 39);
+            this.butVehicles.TabIndex = 1;
+            this.butVehicles.Text = "Vehicles";
+            this.butVehicles.UseVisualStyleBackColor = true;
+            // 
+            // butBookings
+            // 
+            this.butBookings.Location = new System.Drawing.Point(44, 188);
+            this.butBookings.Name = "butBookings";
+            this.butBookings.Size = new System.Drawing.Size(85, 39);
+            this.butBookings.TabIndex = 1;
+            this.butBookings.Text = "Bookings";
+            this.butBookings.UseVisualStyleBackColor = true;
+            // 
+            // butReturns
+            // 
+            this.butReturns.Location = new System.Drawing.Point(44, 244);
+            this.butReturns.Name = "butReturns";
+            this.butReturns.Size = new System.Drawing.Size(85, 39);
+            this.butReturns.TabIndex = 1;
+            this.butReturns.Text = "Returns";
+            this.butReturns.UseVisualStyleBackColor = true;
+            // 
+            // butDamages
+            // 
+            this.butDamages.Location = new System.Drawing.Point(44, 300);
+            this.butDamages.Name = "butDamages";
+            this.butDamages.Size = new System.Drawing.Size(85, 39);
+            this.butDamages.TabIndex = 1;
+            this.butDamages.Text = "Damages";
+            this.butDamages.UseVisualStyleBackColor = true;
+            // 
+            // butLogout
+            // 
+            this.butLogout.Location = new System.Drawing.Point(44, 385);
+            this.butLogout.Name = "butLogout";
+            this.butLogout.Size = new System.Drawing.Size(85, 39);
+            this.butLogout.TabIndex = 1;
+            this.butLogout.Text = "Log out";
+            this.butLogout.UseVisualStyleBackColor = true;
+            // 
+            // lblDashboard
+            // 
+            this.lblDashboard.AutoSize = true;
+            this.lblDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDashboard.Location = new System.Drawing.Point(205, 45);
+            this.lblDashboard.Name = "lblDashboard";
+            this.lblDashboard.Size = new System.Drawing.Size(102, 20);
+            this.lblDashboard.TabIndex = 1;
+            this.lblDashboard.Text = "Dash board";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(28)))), ((int)(((byte)(237)))));
+            this.panel2.Location = new System.Drawing.Point(234, 127);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(28)))), ((int)(((byte)(237)))));
+            this.panel3.Location = new System.Drawing.Point(539, 127);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(28)))), ((int)(((byte)(237)))));
+            this.panel4.Location = new System.Drawing.Point(234, 282);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(28)))), ((int)(((byte)(237)))));
+            this.panel5.Location = new System.Drawing.Point(539, 282);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 100);
+            this.panel5.TabIndex = 5;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.lblDashboard);
+            this.Controls.Add(this.panel1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button butLogout;
+        private System.Windows.Forms.Button butDamages;
+        private System.Windows.Forms.Button butReturns;
+        private System.Windows.Forms.Button butBookings;
+        private System.Windows.Forms.Button butVehicles;
+        private System.Windows.Forms.Button butCustomers;
+        private System.Windows.Forms.Label lblDashboard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
     }
 }
--- CarRentalSystem/Form2.cs
+++ CarRentalSystem/Form2.cs
@@ -21,5 +21,15 @@ namespace CarRentalSystem
         {
 
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
