3fb6637685ffded57c3e3140e95016f0a5e2672a
minababo <minada965@gmail.com>
2024-04-19 16:20:32 +0530

    Added Registration Form

---
+++ CarRentalSystem/Registration.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Registration
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registration));
+            this.label2 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.u_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.con_pass = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.conpass = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Copperplate Gothic Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.label2.Location = new System.Drawing.Point(68, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email Address";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // email
+            // 
+            this.email.BackColor = System.Drawing.Color.White;
+            this.email.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.email.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.Location = new System.Drawing.Point(240, 44);
+            this.email.Margin = new System.Windows.Forms.Padding(10);
+            this.email.Multiline = true;
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(166, 25);
+            this.email.TabIndex = 3;
+            this.email.UseWaitCursor = true;
+            this.email.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // u_name
+            // 
+            this.u_name.BackColor = System.Drawing.Color.White;
+            this.u_name.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.u_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.u_name.Location = new System.Drawing.Point(240, 99);
+            this.u_name.Margin = new System.Windows.Forms.Padding(10);
+            this.u_name.Multiline = true;
+            this.u_name.Name = "u_name";
+            this.u_name.Size = new System.Drawing.Size(166, 25);
+            this.u_name.TabIndex = 5;
+            this.u_name.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Copperplate Gothic Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.label3.Location = new System.Drawing.Point(111, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.White;
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(240, 154);
+            this.password.Margin = new System.Windows.Forms.Padding(10);
+            this.password.Multiline = true;
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(166, 25);
+            this.password.TabIndex = 7;
+            this.password.UseWaitCursor = true;
+            this.password.TextChanged += new System.EventHandler(this.u_name_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Copperplate Gothic Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.label4.Location = new System.Drawing.Point(108, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "User Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox1.Controls.Add(this.conpass);
+            this.groupBox1.Controls.Add(this.con_pass);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.u_name);
+            this.groupBox1.Controls.Add(this.email);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.groupBox1.Location = new System.Drawing.Point(457, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 314);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Get Started";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // con_pass
+            // 
+            this.con_pass.AutoSize = true;
+            this.con_pass.Font = new System.Drawing.Font("Copperplate Gothic Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.con_pass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.con_pass.Location = new System.Drawing.Point(20, 208);
+            this.con_pass.Name = "con_pass";
+            this.con_pass.Size = new System.Drawing.Size(207, 21);
+            this.con_pass.TabIndex = 8;
+            this.con_pass.Text = "Confirm Password";
+            this.con_pass.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.button1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(149, 260);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(144, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(56, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 261);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.label5.Font = new System.Drawing.Font("Copperplate Gothic Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(102, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(255, 33);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Rent Your Car";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.label1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(73, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 62);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Unforgettable journey starts here";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.UseMnemonic = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(50)))), ((int)(((byte)(97)))));
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(434, 414);
+            this.panel1.TabIndex = 13;
+            // 
+            // conpass
+            // 
+            this.conpass.BackColor = System.Drawing.Color.White;
+            this.conpass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.conpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conpass.Location = new System.Drawing.Point(240, 208);
+            this.conpass.Margin = new System.Windows.Forms.Padding(10);
+            this.conpass.Multiline = true;
+            this.conpass.Name = "conpass";
+            this.conpass.PasswordChar = '*';
+            this.conpass.Size = new System.Drawing.Size(166, 25);
+            this.conpass.TabIndex = 10;
+            this.conpass.TextChanged += new System.EventHandler(this.conpass_TextChanged);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 410);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Registration";
+            this.Text = "x";
+            this.Load += new System.EventHandler(this.Registration_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox u_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label con_pass;
+        private System.Windows.Forms.TextBox conpass;
+    }
+}
+
---
+++ CarRentalSystem/Registration.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace Registration
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=RegistrationDB.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(email.Text!="" && password.Text!="" && u_name.Text!="" && con_pass.Text!="")
+            {
+                if (password.Text == conpass.Text)
+                {
+                    int v = check(email.Text);
+                    if (v != 1)
+                    {
+                        try
+                        {
+                            connection.Open();
+                            SqlCommand command = new SqlCommand("insert into RegistrationTbl values(@u_name,@email,@password)", connection);
+                            command.Parameters.AddWithValue("@email", email.Text);
+                            command.Parameters.AddWithValue("@u_name", password.Text);
+                            command.Parameters.AddWithValue("@password", u_name.Text);
+                            command.ExecuteNonQuery();
+                            connection.Close();
+                            MessageBox.Show("Registered Successfully!");
+                            password.Text = "";
+                            email.Text = "";
+                            u_name.Text = "";
+                            conpass.Text = "";
+                        }
+                        catch (SqlException ex)
+                        {
+                            MessageBox.Show("An error occurred during registration. Please try again later.");
+                            Console.WriteLine($"Registration failed: {ex.Message}");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("You are already registered");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Password does not match");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fill in the empty fields!");
+            }
+        }
+
+        int check(string email)
+        {
+            connection.Open();
+            string query = "select count(*) from RegistrationTbl where email='" + email + "'";
+            SqlCommand command= new SqlCommand(query, connection);
+            int v=(int)command.ExecuteScalar();
+            connection.Close();
+            return v;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conpass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void u_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
